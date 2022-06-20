1adfd873dfbbbb0d139751f565a1c54354fe1f23
emirhangulmez <emirhn507@gmail.com>
2022-06-20 19:51:21 +0300

    Initial Commit

---
+++ app/src/main/java/com/emirhan/pricelist/Model/Price.java
@@ -0,0 +1,77 @@
+package com.emirhan.pricelist.Model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Price implements Parcelable {
+
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "image")
+    public byte[] image;
+
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "costPrice")
+    public String costPrice;
+
+    @ColumnInfo(name = "salePrice")
+    public String salePrice;
+
+    @ColumnInfo(name = "quantity")
+    public String quantity;
+
+
+    public Price(String name, String costPrice, String salePrice, String quantity, byte[] image) {
+        this.name = name;
+        this.costPrice = costPrice;
+        this.salePrice = salePrice;
+        this.quantity = quantity;
+        this.image = image;
+    }
+
+
+    protected Price(Parcel in) {
+        id = in.readInt();
+        image = in.createByteArray();
+        name = in.readString();
+        costPrice = in.readString();
+        salePrice = in.readString();
+        quantity = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeByteArray(image);
+        dest.writeString(name);
+        dest.writeString(costPrice);
+        dest.writeString(salePrice);
+        dest.writeString(quantity);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Price> CREATOR = new Creator<Price>() {
+        @Override
+        public Price createFromParcel(Parcel in) {
+            return new Price(in);
+        }
+
+        @Override
+        public Price[] newArray(int size) {
+            return new Price[size];
+        }
+    };
+}
